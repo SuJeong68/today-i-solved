cc8c32df14e5a1e1155ae77c3eb792898180cacd
이수정 <tidh1640@naver.com>
2023-10-26 14:27:45 +0900

    [level 1] Title: K번째수, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -6,9 +6,43 @@ class Solution {
         int[] result = new int[commands.length];
         for (int i = 0; i < commands.length; i++) {
             int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-            Arrays.sort(arr);
+            sort(arr, 0, arr.length - 1);
             result[i] = arr[commands[i][2] - 1];
         }
         return result;
     }
+    
+    // 퀵 정렬
+    public void sort(int[] arr, int start, int end) {
+        // 중간값
+        int mid = (start + end) / 2;
+        int left = start;
+        int right = end;
+        
+        do {
+            while (arr[start] < arr[mid]) {
+                start++;
+            }
+            while (arr[end] > arr[mid]) {
+                end--;
+            }
+            if (start <= end) {
+                swap(arr, start++, end--);
+            }
+        } while (start <= end);
+        
+        if (left < end) {
+            sort(arr, left, end);
+        }
+        if (right > start) {
+            sort(arr, start, right);
+        }
+    }
+    
+    public void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    
 }
