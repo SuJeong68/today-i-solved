2847cd51dcc26231d8551084e3374313a4c2ee3f
이수정 <tidh1640@naver.com>
2023-10-28 22:00:48 +0900

    [level 1] Title: 최소직사각형, Time: 1.74 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,24 +1,12 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        List<int[]> list = Arrays.stream(sizes)
-            .map(card -> {
-                if (card[0] < card[1]) {
-                    int temp = card[0];
-                    card[0] = card[1];
-                    card[1] = temp;
-                }
-                return card;
-            })
-            .collect(Collectors.toList());
-    
         int maxW = 0;
         int maxH = 0;
-        for (int[] card : list) {
-            maxW = Math.max(card[0], maxW);
-            maxH = Math.max(card[1], maxH);
+        for (int[] card : sizes) {
+            maxW = Math.max(maxW, Math.max(card[0], card[1]));
+            maxH = Math.max(maxH, Math.min(card[0], card[1]));
         }
         return maxW * maxH;
     }
