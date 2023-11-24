9e0d7c34943c23a3701ce4e8ad8c11a831488270
이수정 <tidh1640@naver.com>
2023-11-24 13:59:13 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String name) {
+        int updown = 0;
+        int length = name.length();
+        int move = length - 1;
+        
+        for (int i = 0; i < length; i++) {
+            updown += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+            
+            int next = i + 1;
+            while (next < length && name.charAt(next) == 'A') {
+                next++;
+            }
+            
+            move = Math.min(move, i * 2 + (length - next));
+            move = Math.min(move, (length - next) * 2 + i);
+        }
+        return updown + move;
+    }
+}
